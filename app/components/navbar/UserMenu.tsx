f4c09f2c01ed5cf3219974a3e5e27315f3f406e9
Ariiie <oluwafunmilayodada2018@gmail.com>
2023-06-26 00:29:10 +0100

    added the button component

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -31,7 +31,7 @@ const UserMenu = () => {
 			{isOpen && (
 				<div
 					className="absolute rounded-xl shadow-md w-[40vw] md:w-3/4
-				bg-white overflow-hidden right-0 top-12 text-sm"
+				   bg-white overflow-hidden right-0 top-12 text-sm"
 				>
 					<div className="flex flex-col cursor-pointer">
 						<MenuItem onClick={() => {}} label="Login" />
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Nunito } from "next/font/google";
 
 import Navbar from "./components/navbar/Navbar";
+import Modal from "./components/modal/Modal";
 
 const nunito = Nunito({ subsets: ["latin"] });
 
@@ -18,6 +19,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={nunito.className}>
+				<Modal isOpen actionLabel="Submit" title="title" />
 				<Navbar />
 				{children}
 			</body>
